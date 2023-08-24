734327f637dfc5432a27221d886240ee989b1c4c
akshatgg <akshatg9636@gmail.com>
2023-08-24 17:01:41 +0545

    modify

--- Counter app/script.js
+++ Counter app/script.js
@@ -1,2 +1,8 @@
-const decrement=document.getElementById("DecrementB");
-console.log(decrement);
+let decrementb=document.getElementById("Decrement");
+console.log(decrementb.innerText);
+let incrementb=document.getElementById("Increment");
+console.log(incrementb.innerText);
+let resetb=document.getElementById("reset");
+console.log(resetb.innerText);
+let displayvalue=document.getElementById("display")
+console.log(displayvalue.innerText);
