3095dfba743227c0363fd638dae57f4548c4a38d
akshatgg <akshatg9636@gmail.com>
2023-08-24 17:52:23 +0545

    script

--- Counter app/script.js
+++ Counter app/script.js
@@ -6,3 +6,28 @@ let resetb=document.getElementById("reset");
 console.log(resetb.innerText);
 let displayvalue=document.getElementById("display")
 console.log(displayvalue.innerText);
+
+
+function inc(){
+let value=Number(displayvalue.innerText);
+value=value+1;
+return value;
+}
+incrementb.addEventListener("click", inc);
+
+
+function dec(){
+    let value=Number(displayvalue.innerText);
+    value=value-1;
+    return value;
+    }
+    decrementb.addEventListener("click", dec);
+
+
+
+    
+function re(){
+    displayvalue.innerText=0;
+}
+incrementb.addEventListener("click", dec);
+resetb.addEventListener("click",re);
