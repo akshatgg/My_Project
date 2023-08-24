3516c35a84c780288a268a13843b37dd3d8c538f
akshatgg <akshatg9636@gmail.com>
2023-08-24 18:26:37 +0545

    calculator

--- Counter app/script.js
+++ Counter app/script.js
@@ -1,33 +1,32 @@
-let decrementb=document.getElementById("Decrement");
+const decrementb=document.getElementById("Decrement");
 console.log(decrementb.innerText);
-let incrementb=document.getElementById("Increment");
+
+const incrementb=document.getElementById("Increment");
 console.log(incrementb.innerText);
-let resetb=document.getElementById("reset");
+
+const resetb=document.getElementById("reset");
 console.log(resetb.innerText);
-let displayvalue=document.getElementById("display")
+
+const displayvalue=document.getElementById("display");
 console.log(displayvalue.innerText);
 
-
 function inc(){
-let value=Number(displayvalue.innerText);
-value=value+1;
-return value;
+    let value=Number(displayvalue.innerText);
+    value=value+1;
+    displayvalue.innerText=value;
+    return value;
 }
 incrementb.addEventListener("click", inc);
 
-
 function dec(){
     let value=Number(displayvalue.innerText);
-    value=value-1;
+   value =value-1;
+    displayvalue.innerText=value;
     return value;
-    }
-    decrementb.addEventListener("click", dec);
-
-
-
-    
-function re(){
-    displayvalue.innerText=0;
 }
-incrementb.addEventListener("click", dec);
-resetb.addEventListener("click",re);
+decrementb.addEventListener("click", dec);
+
+function re(){
+    displayvalue.innerText = 0;
+}
+resetb.addEventListener("click", re);
