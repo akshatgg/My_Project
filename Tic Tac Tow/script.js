52df3692f4c14027a2c59c8acbdd369f0f4bbd29
akshatgg <akshatg9636@gmail.com>
2023-08-30 01:32:39 +0545

    modify

--- Tic Tac Tow/script.js
+++ Tic Tac Tow/script.js
@@ -1,59 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const boxes = document.querySelectorAll('.box');
-    const gameInfo = document.querySelector('.gameinfo');
-    const resetButton = document.getElementById('reset');
+let turn="X";
+let chance=document.getElementsByClassName("gameinfo")
+//Game logic //
+let boxes=document.getElementsByClassName("box");
+let boxtxt=document.getElementsByClassName('boxtext')
+function changeTurn(){
+return turn ==="X"?"0":X;
+}
+let array=Array.from(boxes);
+array.forEach(e=>{
+e.addEventListener('click',()=>{
+if(boxtxt.innerText===''){
+    boxtxt.innerText=turn;
+    winning();
+    changeTurn();
+    
 
-    let currentPlayer = 'X';
-    let gameOver = false;
-
-    function checkWin() {
-        const lines = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-            [0, 4, 8], [2, 4, 6]            // Diagonals
-        ];
-
-        for (const line of lines) {
-            const [a, b, c] = line;
-            if (boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && boxes[a].textContent === boxes[c].textContent) {
-                return boxes[a].textContent;
-            }
-        }
-
-        return null;
-    }
-
-    function handleClick(event) {
-        const box = event.target;
-        if (!box.textContent && !gameOver) {
-            box.textContent = currentPlayer;
-            const winner = checkWin();
-
-            if (winner) {
-                gameInfo.textContent = `${winner} wins!`;
-                gameOver = true;
-            } else if ([...boxes].every(box => box.textContent !== '')) {
-                gameInfo.textContent = "It's a draw!";
-                gameOver = true;
-            } else {
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                gameInfo.textContent = `Turn for ${currentPlayer}`;
-            }
-        }
-    }
-
-    function resetGame() {
-        boxes.forEach(box => {
-            box.textContent = '';
-        });
-        currentPlayer = 'X';
-        gameOver = false;
-        gameInfo.textContent = `Turn for ${currentPlayer}`;
-    }
-
-    boxes.forEach(box => {
-        box.addEventListener('click', handleClick);
-    });
-
-    resetButton.addEventListener('click', resetGame);
-});
+}
+})
+})
