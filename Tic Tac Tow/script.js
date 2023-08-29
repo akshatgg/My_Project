0043747a72dc89f9b5eb00827d9e527abbe9a6fa
akshatgg <akshatg9636@gmail.com>
2023-08-30 02:02:09 +0545

    script

--- Tic Tac Tow/script.js
+++ Tic Tac Tow/script.js
@@ -1,18 +1,20 @@
-let turn="X";
-let chance=document.getElementsByClassName("gameinfo")
+let turn="X"
+let chance=document.getElementsByClassName(".gameinfo")
 //Game logic //
 let boxes=document.getElementsByClassName("box");
 let boxtxt=document.getElementsByClassName('boxtext')
 function changeTurn(){
-return turn ==="X"?"0":X;
+return turn ==="X"?"0":"X";
 }
 let array=Array.from(boxes);
 array.forEach(element=>{
+    console.log(element);
 element.addEventListener('click',()=>{
-if(boxtxt.innerText===''){
+if( element.querySelector('.boxtext').innerText = turn){
     boxtxt.innerText=turn;
+    console.log(turn.innerText);
     winning();
-    turn=changeTurn();
+    turn=changeTurn()
     if(!gameover()){
         chance.innerText="Turn for"+turn;
     }
