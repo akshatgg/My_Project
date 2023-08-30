be03526aa504644672c4f17f9fc1264f1034412a
akshatgg <akshatg9636@gmail.com>
2023-08-30 13:01:47 +0545

    modify

--- Tic Tac Tow/script.js
+++ Tic Tac Tow/script.js
@@ -1,24 +1,28 @@
-let turn="X"
-let chance=document.getElementsByClassName(".gameinfo")
-//Game logic //
-let boxes=document.getElementsByClassName("box");
-let boxtxt=document.getElementsByClassName('boxtext')
-function changeTurn(){
-return turn ==="X"?"0":"X";
-}
-let array=Array.from(boxes);
-array.forEach(element=>{
-    console.log(element);
-element.addEventListener('click',()=>{
-if( element.querySelector('.boxtext').innerText = turn){
-    boxtxt.innerText=turn;
-    console.log(turn.innerText);
-    winning();
-    turn=changeTurn()
-    if(!gameover()){
-        chance.innerText="Turn for"+turn;
-    }
+let turn = "X";
+let chance = document.querySelector(".gameinfo"); // Use querySelector to get the single element
 
+// Game logic //
+let boxes = document.getElementsByClassName("box");
+let boxtxt = document.getElementsByClassName('boxtext');
+
+function changeTurn() {
+    return turn === "X" ? "O" : "X"; // Use "O" instead of "0", and wrap "X" and "O" in quotes
 }
-})
-})
+
+let array = Array.from(boxes);
+
+array.forEach(element => {
+    console.log(element);
+    element.addEventListener('click', () => {
+        if (element.querySelector('.boxtext').innerText === '') { // Use element.querySelector to get the specific box's text
+            element.querySelector('.boxtext').innerText = turn;
+            console.log(turn); // Just use "turn" instead of "turn.innerText"
+            winning();
+            turn = changeTurn();
+            if (!gameover()) {
+                chance.innerText = "Turn for " + turn; // Add a space after "for"
+            }
+        }
+    });
+});
+    
