a2aa12d091d8cebf71302a576ecfb122d360e991
akshatgg <akshatg9636@gmail.com>
2023-09-05 18:55:23 +0545

    modify

--- Tic Tac Tow/script.js
+++ Tic Tac Tow/script.js
@@ -4,7 +4,7 @@ let chance = document.querySelector(".gameinfo");
 // Game logic //
 let boxes = document.getElementsByClassName("box");
 let boxtxt = document.getElementsByClassName("boxtext");
-
+let gameover =false;
 function changeTurn() {
     return turn === "X" ? "O" : "X"; // Use "O" instead of "0", and wrap "X" and "O" in quotes
 }
@@ -19,10 +19,30 @@ aray.forEach(function(element) {
             console.log(turn); // Just use "turn" instead of "turn.innerText"
             winning();
             turn = changeTurn();
-            if (!gameover()) {
+            if (!gameover) {
                 chance.innerText = "Turn for " + turn; // Add a space after "for"
             }
         }
     });
 });
+//check for winning//
+function winning(){
+let wins =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+wins.forEach(function(e){
+    if((boxtxt[e[0]].innerText === boxtxt[e[1]].innerText) && ( boxtxt[e[1]].innerText === boxtxt[e[2]].innerText )&& (boxtxt[e[0]].innerText !=="")) {
+        chance.innerText = boxtxt[e[0]].innerText +" Wins";
+        gameover=true;
+    }
+})
+}
+
         
